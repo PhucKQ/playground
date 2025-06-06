6f193667adf717440f6c7f76cd48c63be526400d
Phúc KQ <91537221+PhucKQ@users.noreply.github.com>
2025-06-06 13:53:41 +0700

    Update vite.config.js
    update base path

--- timetabled/vite.config.js
+++ timetabled/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './CodePathWEB102-playground/timetabled/',
+  base: './playground/timetabled/',
   plugins: [react()],
   build: {
     outDir: 'dist',
